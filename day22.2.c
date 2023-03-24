5420d1b26881912492468376bd8b66c2da1589e1
dhairya6570 <126230153+dhairya6570@users.noreply.github.com>
2023-03-24 11:25:13 -0400

    Add files via upload

---
+++ day22.2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+int main (){
+	
+	char email[50];
+	char pass[20];
+	
+	char correctemail[] = "dhairyapatel20104@gmail.com";
+	char correctpass[] = "Paresh6570";
+	
+	printf("Enter email : ");
+	scanf("%s",email);
+	
+	printf("Enter pass : ");
+	scanf("%s",pass);
+	
+	if(strcmp(email , correctemail) == 0 && strcmp(email , correctemail) == 0){
+		printf("Login is successful");
+	}
+	else{
+		printf("Pls try again");
+	}
+	
+	return 0 ;
+}
